f7f93f07df1ba384d223a01321a845582252e0c6
thorel <thorel@31c93ca4-b26e-45ae-9a3b-1104ce8f2522>
2015-01-07 11:26:31 +0000

    1er commit

---
+++ ic-2014/mdu/contact-service/src/main/java/model/Contact.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.io.Serializable;
+
+/**
+ * Representation objet d'un contact
+ * @author duboism
+ *
+ */
+public class Contact implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**Nom du contact*/
+	private String nom;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Contact)) {
+			return false;
+		}
+		Contact other = (Contact) obj;
+		if (nom == null) {
+			if (other.nom != null) {
+				return false;
+			}
+		} else if (!nom.equals(other.nom)) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+	
+}
