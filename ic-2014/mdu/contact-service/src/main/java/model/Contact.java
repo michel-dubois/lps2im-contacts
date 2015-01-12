35aa756f79ebd238c56d509684680b2edb7c36ca
duboism <duboism@31c93ca4-b26e-45ae-9a3b-1104ce8f2522>
2015-01-12 17:24:41 +0000

    test + PMD CheckStyle JavaCC Findbugs

--- ic-2014/mdu/contact-service/src/main/java/model/Contact.java
+++ ic-2014/mdu/contact-service/src/main/java/model/Contact.java
@@ -4,56 +4,53 @@ import java.io.Serializable;
 
 /**
  * Representation objet d'un contact
+ * 
  * @author duboism
- *
+ * 
  */
 public class Contact implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	/**Nom du contact*/
-	private String nom;
+    private static final long serialVersionUID = 1L;
 
-	public String getNom() {
-		return nom;
-	}
+    /** Nom du contact */
+    private String nom;
 
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
+    public String getNom() {
+        return nom;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
-		return result;
-	}
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (!(obj instanceof Contact)) {
-			return false;
-		}
-		Contact other = (Contact) obj;
-		if (nom == null) {
-			if (other.nom != null) {
-				return false;
-			}
-		} else if (!nom.equals(other.nom)) {
-			return false;
-		}
-		return true;
-	}
-	
-	
-	
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Contact)) {
+            return false;
+        }
+        Contact other = (Contact) obj;
+        if (nom == null) {
+            if (other.nom != null) {
+                return false;
+            }
+        } else if (!nom.equals(other.nom)) {
+            return false;
+        }
+        return true;
+    }
+
 }
--- ic-2014/mdu/contact-service/src/main/java/service/ContactService.java
+++ ic-2014/mdu/contact-service/src/main/java/service/ContactService.java
@@ -1,58 +1,91 @@
 package service;
 
-import org.apache.commons.lang3.StringUtils;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import model.Contact;
+
+import org.apache.commons.lang3.StringUtils;
+
 import dao.ContactDaoImpl;
 import dao.IContactDao;
 import exception.ContactInexistantException;
 import exceptions.ContactException;
 
+/**
+ * Service de creation de contacts
+ * 
+ * @author duboism
+ * 
+ */
 public class ContactService {
 
-	private IContactDao contactDao = new ContactDaoImpl();
-	
-	/**
-	 * Méthode permettant d'ajouter un contact 
-	 * @param nom
-	 * @throws ContactException si le nom passé est déjà présent en base de données
-	 */
-	public void creerContact(String nom) throws ContactException{
-		int taille=StringUtils.trimToEmpty(nom).length();
-		
-//		if(nom == null || nom.trim().length() < 3 || nom.trim().length()>40){
-		if  (taille < 3 || taille>40){
-		   throw new IllegalArgumentException("Le nom doit être compris entre 3 et 40 caractères");
-		}
-		
-		if(contactDao.rechercherContact(nom) != null){
-			throw new ContactException("Un contact avec le nom "+nom+" existe déjà en base de données");
-		}
-		
-		Contact contact = new Contact();
-		contact.setNom(nom);
-		contactDao.creerContact(contact);
-	}
+    private static final int MAX_SIZE = 40;
+    private static final int MIN_SIZE = 3;
+    private IContactDao contactDao = new ContactDaoImpl();
 
-	public Contact modifierNomContact(String ancienNom, String nouveauNom) throws ContactInexistantException, ContactException{
-		
-		Contact contactBase = contactDao.rechercherContact(ancienNom);
-		if(contactBase == null){
-			throw new ContactInexistantException();
-		}
-		
-		if(contactDao.rechercherContact(nouveauNom) != null){
-			throw new ContactException("Un contact avec le nom "+nouveauNom+" existe déjà en base de données");
-		}
-		
-		return contactDao.updateContact(contactBase, nouveauNom);
-	}
-	
-	public void setContactDao(IContactDao contactDao) {
-		this.contactDao = contactDao;
-	}
-	
-	
-	
-	
+    /**
+     * Méthode permettant d'ajouter un contact
+     * 
+     * @param nom
+     * @throws ContactException si le nom passé est déjà présent en base de données
+     */
+    public void creerContact(String nom) throws ContactException {
+        int taille = StringUtils.trimToEmpty(nom).length();
+
+        // if(nom == null || nom.trim().length() < 3 || nom.trim().length()>40){
+        if (taille < MIN_SIZE || taille > MAX_SIZE) {
+            throw new IllegalArgumentException("Le nom doit être compris entre 3 et 40 caractères");
+        }
+
+        if (contactDao.rechercherContact(nom) != null) {
+            throw new ContactException("Un contact avec le nom " + nom + " existe déjà en base de données");
+        }
+
+        Contact contact = new Contact();
+        contact.setNom(nom);
+        contactDao.creerContact(contact);
+    }
+
+    /**
+     * 
+     * @param ancienNom
+     * @param nouveauNom
+     * @return
+     * @throws ContactInexistantException
+     * @throws ContactException
+     */
+    public Contact modifierNomContact(String ancienNom, String nouveauNom) throws ContactInexistantException,
+            ContactException {
+
+        Contact contactBase = contactDao.rechercherContact(ancienNom);
+        if (contactBase == null) {
+            throw new ContactInexistantException();
+        }
+
+        if (contactDao.rechercherContact(nouveauNom) != null) {
+            throw new ContactException("Un contact avec le nom " + nouveauNom + " existe déjà en base de données");
+        }
+
+        return contactDao.updateContact(contactBase, nouveauNom);
+    }
+
+    public void setContactDao(IContactDao contactDao) {
+        this.contactDao = contactDao;
+    }
+
+    public void methodebug() {
+        Contact contact = null;
+        if (Calendar.getInstance().get(Calendar.DAY_OF_MONTH) > 20) {
+            contact = new Contact();
+        }
+        contact.setNom("nom");
+        contact.getNom().replace("o", "u");
+        if (contact.getNom().equals(new ArrayList<String>())) {
+            System.out.println("Coucou");
+        }
+        while (true) {
+            System.out.println("Coucou");
+        }
+    }
 }
