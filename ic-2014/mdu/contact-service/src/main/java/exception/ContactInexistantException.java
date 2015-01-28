b281addcdf7faa7badd744b7a36defb50ef5241f
duboism <duboism@31c93ca4-b26e-45ae-9a3b-1104ce8f2522>
2015-01-28 14:06:29 +0000

    corrections

---
+++ ic-2014/mdu/contact-service/src/main/java/exception/ContactInexistantException.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class ContactInexistantException extends Exception {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 937125556922774122L;
+
+    public ContactInexistantException(String string) {
+        super(string);
+    }
+
+}
--- ic-2014/mdu/contact-service/src/main/java/service/ContactService.java
+++ ic-2014/mdu/contact-service/src/main/java/service/ContactService.java
@@ -1,66 +1,71 @@
 package service;
 
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.Collection;
+import java.util.List;
 
 import model.Contact;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
 
 import dao.ContactDaoImpl;
 import dao.IContactDao;
+import exception.ContactException;
 import exception.ContactInexistantException;
-import exceptions.ContactException;
 
 /**
- * Service de creation de contacts
+ * Service de gestion des contacts
  * 
  * @author duboism
  * 
  */
 public class ContactService {
 
-    private static final int MAX_SIZE = 40;
-    private static final int MIN_SIZE = 3;
+    private static final int TAILLE_MAX_NOM = 40;
+    private static final int TAILLE_MIN_NOM = 3;
+
     private IContactDao contactDao = new ContactDaoImpl();
 
     /**
      * Méthode permettant d'ajouter un contact
      * 
-     * @param nom
+     * @param nom nom du contact a créer
      * @throws ContactException si le nom passé est déjà présent en base de données
      */
     public void creerContact(String nom) throws ContactException {
         int taille = StringUtils.trimToEmpty(nom).length();
-
-        // if(nom == null || nom.trim().length() < 3 || nom.trim().length()>40){
-        if (taille < MIN_SIZE || taille > MAX_SIZE) {
+        if (taille < TAILLE_MIN_NOM || taille > TAILLE_MAX_NOM) {
             throw new IllegalArgumentException("Le nom doit être compris entre 3 et 40 caractères");
         }
 
         if (contactDao.rechercherContact(nom) != null) {
             throw new ContactException("Un contact avec le nom " + nom + " existe déjà en base de données");
         }
-
         Contact contact = new Contact();
         contact.setNom(nom);
         contactDao.creerContact(contact);
     }
 
     /**
+     * Méthode qui permet de modifier un contact
      * 
-     * @param ancienNom
-     * @param nouveauNom
-     * @return
-     * @throws ContactInexistantException
-     * @throws ContactException
+     * @param ancienNom ancien nom du contact
+     * @param nouveauNom nouveau nom
+     * @return contact modifié
+     * @throws ContactInexistantException exception si le contact n'existe plus en base
+     * @throws ContactException exception si le nouveau nom est déjà utilisé
      */
     public Contact modifierNomContact(String ancienNom, String nouveauNom) throws ContactInexistantException,
             ContactException {
 
         Contact contactBase = contactDao.rechercherContact(ancienNom);
         if (contactBase == null) {
-            throw new ContactInexistantException("Le contact n'a pas été trouvé");
+            throw new ContactInexistantException("Un contact avec le nom " + ancienNom
+                    + " n'existe pas en base de données");
         }
 
         if (contactDao.rechercherContact(nouveauNom) != null) {
@@ -74,18 +79,21 @@ public class ContactService {
         this.contactDao = contactDao;
     }
 
-    public void methodebug() {
-        Contact contact = null;
-        if (Calendar.getInstance().get(Calendar.DAY_OF_MONTH) > 20) {
-            contact = new Contact();
+    public Collection<String> recupererListeNomContact() {
+
+        List<Contact> listeContact = contactDao.recupererListe();
+        if (CollectionUtils.isNotEmpty(listeContact)) {
+            // Transformation de mon objet contact pour récupérer le nom
+            return Collections2.transform(listeContact, new Function<Contact, String>() {
+
+                public String apply(Contact contact) {
+                    return contact.getNom();
+                }
+
+            });
+        } else {
+            return new ArrayList<String>();
         }
-        contact.setNom("nom");
-        contact.getNom().replace("o", "u");
-        if (contact.getNom().equals(new ArrayList<String>())) {
-            System.out.println("Coucou");
-        }
-        while (true) {
-            System.out.println("Coucou");
-        }
+
     }
 }
