a5204f78704eb1316df8dbe22fdfcedac9e6e5f0
duboism <duboism@31c93ca4-b26e-45ae-9a3b-1104ce8f2522>
2015-01-28 17:43:59 +0000

    remplace les accents par des sans accents encodage utf8

--- ic-2014/mdu/contact-service/src/main/java/dao/IContactDao.java
+++ ic-2014/mdu/contact-service/src/main/java/dao/IContactDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 import model.Contact;
 
 /**
- * Interface du DAO permettant de gérer les contacts
+ * Interface du DAO permettant de gerer les contacts
  * 
  * @author duboism
  * 
@@ -13,24 +13,24 @@ import model.Contact;
 public interface IContactDao {
 
     /**
-     * Méthode qui permet de rechercher un contact a partir de son nom
+     * Methode qui permet de rechercher un contact a partir de son nom
      * 
-     * @param nom nom du contact recherché
+     * @param nom nom du contact recherche
      * @return contact correspondant, si aucun ne correspond retourne <code>null</code>
      */
     Contact rechercherContact(String nom);
 
     /**
-     * Méthode permettant d'insérer en base de données le contact
+     * Methode permettant d'inserer en base de donnees le contact
      * 
      * @param contact contact a ajouter en base
      */
     void creerContact(Contact contact);
 
     /**
-     * Mise à jour du nom d'un contact
+     * Mise a jour du nom d'un contact
      * 
-     * @param contact contact a mettre à jour
+     * @param contact contact a mettre a jour
      * @param nom nouveau nom du contact
      * @return contact a jour avec le nouveau nom
      */
